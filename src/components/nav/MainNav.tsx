b8fbdf5a4a89d41d44a2b747ad2be01be56ae634
Arpit Bansal <arpit.bansal2010@gmail.com>
2022-12-13 13:44:04 +0530

    update: traefik enterprise description

--- src/components/nav/MainNav.tsx
+++ src/components/nav/MainNav.tsx
@@ -42,7 +42,7 @@ const MainNav: React.FC = () => {
           <Grid sx={{ gridTemplateColumns: 'repeat(2, 364px)' }}>
             <Product
               title="Traefik Enterprise"
-              description="All-in-one ingress, API management, and service mesh"
+              description="All-in-one ingress, API gateway, and service mesh"
               logo={<TraefikEnterpriseLogo />}
               url="https://traefik.io/traefik-enterprise/"
               color="#337fe6"
