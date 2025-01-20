30727c6e719b9af8cb720d7458bcc22bc4feb82e
harshad-tahiliani <harshad@codalent.com>
2025-01-20 21:23:54 +0530

    feat: product navigation update

--- src/components/nav/Product.tsx
+++ src/components/nav/Product.tsx
@@ -4,60 +4,73 @@ import { Box, Text, Heading } from '@containous/faency'
 import Link from 'components/Link'
 
 type NavProductProps = {
-  title: string
+  title?: string
   description?: string
   links?: {
     title: string
     url: string
     icon: any
     description: string
-    tagColor?: string
-    tag?: string
     external: boolean
     badge?: string
   }[]
-  bgImage: string
+  subLinks?: {
+    title: string
+    url: string
+    icon: ReactNode
+    external: boolean
+    badge?: string
+  }[]
+  bgImage?: string
 }
 
 type NavbarLinkProps = {
   url: string
   external: boolean
   children: ReactNode
+  styles?: any
 }
 
-const Product = ({ bgImage, title, description, links }: NavProductProps) => (
-  <Box sx={{ p: '24px', backgroundColor: '#fff', position: 'relative' }}>
-    <img
-      src={bgImage}
-      width={320}
-      height={84}
-      alt=""
-      style={{
-        position: 'absolute',
-        paddingBottom: '0px',
-        bottom: '0',
-        left: '0',
-        right: '0',
-      }}
-    />
-    <Heading
-      as="span"
-      sx={{
-        fontSize: '13px',
-        variant: 'default',
-        lineHeight: '1.23',
-        color: '03192d',
-        letterSpacing: '2.36px',
-        fontWeight: '500',
-        textTransform: 'uppercase',
-        fontFamily: 'Rubik',
-      }}
-    >
-      {title}
-    </Heading>
-    <Text as="p" sx={{ fontSize: '14px', lineHeight: '1.14', color: '#4e5e6c', margin: '8px 0px 16px' }}>
-      {description}
-    </Text>
+const Product = ({ bgImage, title, description, links, subLinks = [] }: NavProductProps) => (
+  <Box sx={{ p: title ? '24px' : '16px 16px 48px', backgroundColor: '#fff', position: 'relative' }}>
+    {bgImage && (
+      <img
+        src={bgImage}
+        width={320}
+        height={84}
+        alt=""
+        style={{
+          position: 'absolute',
+          paddingBottom: '0px',
+          bottom: '0',
+          left: '0',
+          right: '0',
+        }}
+      />
+    )}
+    {title && (
+      <Heading
+        as="p"
+        sx={{
+          fontSize: '13px',
+          variant: 'default',
+          lineHeight: '1.23',
+          color: '03192d',
+          letterSpacing: '2.36px',
+          fontWeight: '500',
+          textTransform: 'uppercase',
+          fontFamily: 'Rubik',
+          marginBottom: '16px',
+        }}
+      >
+        {title}
+      </Heading>
+    )}
+    {description && (
+      <Text as="p" sx={{ fontSize: '14px', lineHeight: '1.14', color: '#4e5e6c', margin: '8px 0px 16px' }}>
+        {description}
+      </Text>
+    )}
     <Links>
       {links &&
         links.map((link) => (
@@ -67,7 +80,6 @@ const Product = ({ bgImage, title, description, links }: NavProductProps) => (
               <div>
                 <NavLinkTitle>
                   <span>{link.title}</span>
-                  {link.tag && <NavLinkTag tagColor={link.tagColor}>{link.tag}</NavLinkTag>}
                   {link.badge && <NavLinkBadge>{link.badge}</NavLinkBadge>}
                 </NavLinkTitle>
                 <NavLinkDescription>{link.description}</NavLinkDescription>
@@ -76,20 +88,36 @@ const Product = ({ bgImage, title, description, links }: NavProductProps) => (
           </li>
         ))}
     </Links>
+    {subLinks.length ? (
+      <SubLinks>
+        {subLinks.map((link) => (
+          <li key={title + link.title}>
+            <NavbarLink external={link.external} url={link.url} styles={{ textDecoration: 'none', display: 'flex' }}>
+              <div>{link.icon}</div>
+              <div>
+                <span>{link.title}</span>
+              </div>
+            </NavbarLink>
+          </li>
+        ))}
+      </SubLinks>
+    ) : null}
   </Box>
 )
 
-const NavbarLink: React.FC<NavbarLinkProps> = ({ children, external, url }) => {
+const NavbarLink: React.FC<NavbarLinkProps> = ({ children, external, url, styles }) => {
+  const style = styles ? { ...styles } : { textDecoration: 'none', padding: '8px', borderRadius: '8px' }
+
   if (external) {
     return (
-      <a href={url} style={{ textDecoration: 'none', padding: '8px', borderRadius: '8px' }}>
+      <a href={url} style={style}>
         {children}
       </a>
     )
   }
 
   return (
-    <Link href={url} style={{ textDecoration: 'none', padding: '8px', borderRadius: '8px' }}>
+    <Link href={url} style={style}>
       {children}
     </Link>
   )
@@ -98,7 +126,7 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ children, external, url }) => {
 const Links = styled.ul`
   list-style: none;
   padding: 0;
-  margin: 16px 0 0;
+  margin: 0;
   position: relative;
   margin li {
     padding: 0;
@@ -123,6 +151,36 @@ const Links = styled.ul`
     }
   }
 `
+
+const SubLinks = styled.ul`
+  list-style: none;
+  padding: 0;
+  padding-left: 56px;
+  margin: 8px 0 0;
+  position: relative;
+
+  li + li {
+    margin-top: 8px;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  a {
+    font-size: 14px;
+    font-weight: 400;
+    gap: 8px;
+    line-height: 1.43;
+    color: #354757;
+
+    &:hover {
+      font-weight: 600;
+    }
+  }
+`
+
 const NavLinkIcon = styled.div`
   margin-right: 16px;
   flex-shrink: 0;
@@ -158,19 +216,6 @@ const NavLinkDescription = styled.span`
   display: block;
   font-weight: 400;
   color: '#4e5e6c';
-`
-
-const NavLinkTag = styled.div<{ tagColor?: string }>`
-  font-size: 11px;
-  font-weight: 500;
-  line-height: normal;
-  height: 16px;
-  border-radius: 2px;
-  color: ${({ tagColor }) => tagColor || '#7f8c2b'};
-  margin-left: 8px;
-  padding: 1px 3px 1px;
-  border: solid 1px ${({ tagColor }) => (tagColor ? 'rgba(177, 64, 245, 0.5)' : 'rgba(127, 140, 43, 0.5)')};
-  background-color: ${({ tagColor }) => (tagColor ? 'rgba(177, 64, 245, 0.15)' : 'rgba(127, 140, 43, 0.15)')};
 `
 
 export default Product
